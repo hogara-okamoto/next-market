4e7d5c743ab48f3bf85bea866b2aa90331eb1430
Hogara Okamoto <okamoto@100dreams.co.jp>
2024-11-05 06:18:37 +0900

    .

--- app/api/item/readall/route.js
+++ app/api/item/readall/route.js
@@ -5,8 +5,8 @@ import { ItemModel } from "../../../utils/schemaModels";
 export async function GET(){
     try {
         await connectDB()
-        const allitems = await ItemModel.find()
-        return NextResponse.json({message: "アイテム読み取り成功（オール）", allitems: allitems})
+        const allItems = await ItemModel.find()
+        return NextResponse.json({message: "アイテム読み取り成功（オール）", allItems: allItems})
     } catch {
         console.error("Error reading items:", error);
         return NextResponse.json({message: "アイテム読み取り失敗（オール）"})
