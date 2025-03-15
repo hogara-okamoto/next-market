547d6cc2f0f7ad2014f38e090acd5b1d65626655
Hogara Okamoto <okamoto@100dreams.co.jp>
2025-03-16 04:00:30 +0900

    Binded element children implicitly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
+import { ReactNode } from "react";
 import "./globals.css"
 import Header from "./components/header"
 import Footer from "./components/footer"
 
-const RootLayout = ({children}) => {
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+const RootLayout = ({children}: RootLayoutProps) => {
   return (
     <html lang="en">
       <body>
