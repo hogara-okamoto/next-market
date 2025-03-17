4be80dbdef4c7d514ad1fd30274c616b0754719a
Hogara Okamoto <okamoto@100dreams.co.jp>
2025-03-18 04:39:11 +0900

    Added an alert message for a duplicated user registration

--- app/api/user/register/route.js
+++ app/api/user/register/route.js
@@ -3,9 +3,23 @@ import connectDB from "../../../utils/database"
 import { UserModel } from "../../../utils/schemaModels"
 
 export async function POST(request) {
-    const reqBody = await request.json()
     try {
         await connectDB()
+        
+        const reqBody = await request.json()
+        const { email } = reqBody;
+        
+        // Check if item already exists
+        const existingUser = await UserModel.findOne({ email });
+        
+        //  if exists, 409 stops the process and return the response to the front end.
+        if (existingUser) {
+            return NextResponse.json(
+                { success: false, message: "ユーザーはすでに登録されています" },
+                { status: 409 }
+            );
+        }
+
         await UserModel.create(reqBody)
         return NextResponse.json({message: "ユーザー登録成功"})
     } catch {
